12a2a4732cb4b06e16bdab8782dc0d3acd6e911e
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-04-18 15:43:02 +0530

    UntilLoop

---
+++ Session-11/UntilLoop3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Checking For Internet Connections..."
+
+until ping -c 1 google.com &>/dev/null; do
+    echo "No Internet Connection. Retrying in 5 seconds..."
+    sleep 5
+done
+echo "Internet Connection Available."
