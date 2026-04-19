3faaa06b6e79f0c4fff1d69193116fba4e1d95e2
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-04-19 13:52:43 +0530

    Python Basics

---
+++ Session-12/01. Basic.py
@@ -0,0 +1,19 @@
+#1. print Message
+print("Hello , DevOps!")
+
+#2. Variables
+# string
+name="Nikunj Soni - DevOps Trainer"
+print(name)
+
+#3. Integer
+task_completed=100
+print(task_completed)
+
+#4. float
+cpu_usage=75.5
+print(cpu_usage)
+
+# 5. Boolean
+is_server_up=True   
+print(is_server_up)
---
+++ Session-12/02. LogicalOperators.py
@@ -0,0 +1,7 @@
+
+a = True
+b = False
+print ("a and b:",a and b)  # Logical AND   (Both True for True)
+print ("a or b:",a or b)    # Logical OR    (At least one True for True)
+print ("not a:",not a)      # Logical NOT   (negates the value, True becomes False and vice versa)
+print ("not b:",not b)      # Logical NOT   (negates the value, True becomes False and vice versa)
