367605d6435b61584278eae34e122138db89c44a
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-04-19 14:18:54 +0530

    Python Basics

---
+++ Session-12/03. AsignmentalOperator.py
@@ -0,0 +1,13 @@
+a = 10
+
+a += 2  # 10+2=12
+print(a)  # Output: 12
+
+a *=3  # 12*3=36
+print(a)  # Output: 36
+
+a -= 3 # 36-3= 33
+print(a)
+
+a /=3 # 33/3=11.0
+print(a)  # Output: 11.0
---
+++ Session-12/04. ArithmaticOperator.py
@@ -0,0 +1,10 @@
+a = 5
+b = 2
+
+print ("Addition(a+b):",a+b)        # Addition
+print ("Subtraction(a-b):",a-b)     # Subtraction
+print ("Multiplication (a*b):",a*b) # Multiplication
+print ("Division (a/b):",a/b)       # Division
+print ("Modulus (a%b):",a%b)        # Modulus (Remainder)
+print ("Exponential (a**b):",a**b)  # Exponentiation (a raised to the power of b)
+print ("Floor Division (a//b):",a//b) # Floor Division (Quotient without the remainder)
