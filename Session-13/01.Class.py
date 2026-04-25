55c5f218e3e974dc9f720a156ad221c1bc357b85
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-04-25 13:57:13 +0530

    OOP IN PYTHON

--- Session-13/01.Class.py
+++ Session-13/01.Class.py
@@ -4,5 +4,6 @@ class Server:
             self.ip=ip
     def status(self):
         print(f"{self.name} at {self.ip} is running.....! ")
+        
 web_server=Server("Webserver","198.162.1.10")
 web_server.status()
--- Session-13/02.Task.py
+++ Session-13/02.Task.py
@@ -1,34 +1,3 @@
-# Object Oriented Programming
-- object Oriented Programming lets you structure your code using classes and Objects
-- class is a blueprint of the structure
-- object is a part of that blueprint
-- Example:
-```
-class Server:
-    def __init__(self,name,ip):
-            self.name=name
-            self.ip=ip
-    def status(self):
-        print(f"{self.name} at {self.ip} is running.....! ")
-        
-web_server=Server("Webserver","198.162.1.10")
-web_server.status()
-
-```
-- Here Server is a class : Blueprint
-- __init__ => RUns when you create a new Server
-- self.name,self.ip => These are the server's Details (Attributes)
-- status()=> a function (Method) will print the name and Ip Address
-- web_server=> Server(..) Creating and Actual Server (Object)
-- web_server.status=> calling method of the class
-## Task
-- Write a Program to call different methods of a class in Python using Class and Object
-```
-Time: 5 minutes
-Hint: Create a Class that stores information about a server and has a method to check if it need an update or not
-```
-## Solution
-```
 class Server:
     def __init__(self,name,current_version,latest_version):
         self.name=name
@@ -62,5 +31,3 @@ server1 = Server("Production Server",1.0,2.0)
 server1.display_info()
 server1.check_upadte()
 server1.update_server()
-
-```
