0d37da8dbc7f04b47e09769b376d410b976c0e4b
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-05-03 14:53:45 +0530

    NumPy,Pandas,shutil

---
+++ Session-16/03.backup.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+from datetime import datetime
+
+source="/mnt/d/test_folder" #for windows use: "D:/test_folder"
+backup_dir="/mnt/d/backups" # for windows use: "D:/backups"
+timestamp=datetime.now().strftime("%Y%m%d%H%M%S")
+backup_file=os.path.join(backup_dir,f"backup_{timestamp}.zip")
+
+shutil.make_archive(backup_file.replace('.zip',''),'zip',source)
+print(f"Backup created at: {backup_file}")
