26823256f53d4fb5a5bbecaa751a9f72238629b8
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-05-09 15:41:39 +0530

    Git WorkFlow

--- Session-17/GitWorkFlow-1/test_main.py
+++ Session-17/GitWorkFlow-1/test_main.py
@@ -1,7 +1,15 @@
 import unittest
-from main import add
+from main import add,subtract,multiply
 
 class TestMain(unittest.TestCase):
     def test_add(self):
         assert add(2,3) == 5
         assert add(-1,1) == 0
+        
+    def test_subtract(self):
+        assert subtract(10, 5) == 5
+        assert subtract(10, 10) == 0
+
+    def test_multiply(self):
+        assert multiply(10, 5) == 50
+        assert multiply(2, 3) == 6
