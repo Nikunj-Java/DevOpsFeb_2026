268a5159f5840a9f0de1117d959c217852ce74dc
sonam-niit <sonam.gravity@gmail.com>
2026-05-30 15:19:42 +0530

    Selenium Testing

--- Session-22/02.Facebook.py
+++ Session-22/02.Facebook.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install())
@@ -17,6 +18,9 @@ password= driver.find_element(By.NAME,"pass")
 #print("Password Field Required")
 password.send_keys("password@1123")
 
+button= driver.find_element(By.XPATH,'//*[@id="login_form"]/div/div[1]/div/div[3]/div/div/div')
+button.click()
 
+input("Press Enter to close the browser...")
 
 #driver.quit()
