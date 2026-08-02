f6d3a35e63e0e5ac4a89a72e8ed6ce9f309b7e1b
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-08-02 15:08:37 +0530

    Terrafrm S3Bucket

--- Session-41/s3-bucket-terraform/main.tf
+++ Session-41/s3-bucket-terraform/main.tf
@@ -49,3 +49,20 @@ resource "aws_s3_bucket_public_access_block" "bucket_block" {
   restrict_public_buckets = false
 }
 
+# This policy will allow public read access to the objects in the S3 bucket.
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.versioned_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.versioned_bucket.arn}/*"
+      }
+    ]
+  })
+}
